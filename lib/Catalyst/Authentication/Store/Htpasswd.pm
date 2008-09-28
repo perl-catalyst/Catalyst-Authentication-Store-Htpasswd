e82cdd07bbea6a1c440034d6c6b5adf89a62b055
Tomas Doran <bobtfish@bobtfish.net>
2008-09-28 16:00:34 +0000

    Checking in changes prior to tagging of version 1.000.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 7764)
    +++ Changes	(local)
    @@ -1,5 +1,7 @@
    -0.03  XXX XXX  x XX:XX:XX GMT 2008
    +1.000  Sun Sept  28 16:58:30 GMT 2008
    Change from Module::Build to Module::Install
    +    - Update to the new authentication framework.
    +    - Bump revision number due to moving to new repository layout.
    
    0.02  Wed Feb  8 01:18:17 CET 2006
    work properly again with roles

--- lib/Catalyst/Authentication/Store/Htpasswd.pm
+++ lib/Catalyst/Authentication/Store/Htpasswd.pm
@@ -9,7 +9,7 @@ use Authen::Htpasswd;
 use Catalyst::Authentication::Store::Htpasswd::User;
 use Scalar::Util qw/blessed/;
 
-our $VERSION = '0.03';
+our $VERSION = '1.000';
 
 BEGIN { __PACKAGE__->mk_accessors(qw/file/) }
 
